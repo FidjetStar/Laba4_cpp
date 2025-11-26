ee0ddbcbdba2661aa83a0b5f4d3502b420def9bd
Vova Riskov <154013808+FidjetStar@users.noreply.github.com>
2025-11-26 16:50:35 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,9 +16,11 @@ int main() {
     do {
         cout << "1. Заполнить двумерный массив с клавиатуры" << endl;
         cout << "2. Заполнить двумерный массив случайными числами" << endl;
-        cout << "3. Заполнить двумерный массив из файла" << endl;
-        cout << "4. Задание Matrix29" << endl;
-        cout << "5. Задание Matrix59" << endl;
+        cout << "3. Заполнить двумерный массив из файла (b.txt)" << endl;
+        cout << "4. Задание Matrix29 - ответы выводятся в консоль и в файл " << endl;
+        cout << "5. Задание Matrix59 - ответы выводятся в консоль и в файл " << endl;
+        cout << "6. Задание DArray3(Матрица должна быть квадратная) - ответы выводятся в консоль и в файл " << endl;
+        cout << "7. Задание DArray16 - ответы выводятся в консоль и в файл " << endl;
         cout << "777. Выход" << endl;
         cin >> choice;
         switch (choice) {
@@ -38,14 +40,24 @@ int main() {
                 fill_file(matrix, filename);
                 break;
             }
-            case 4: {
+            case 4: { // 29 //
                 matrix29Console(matrix);
                 matrix29File(matrix);
                 break;
             }
-            case 5: {
+            case 5: { // 59 //
                 matrix59Console(matrix);
                 matrix59File(matrix);
+                break;
+            }
+            case 6: {
+                darray3console(matrix);
+                darray3file(matrix);
+                break;
+            }
+            case 7: {
+                darray16console(matrix);
+                darray16file(matrix); 
                 break;
             }
             case 777:
@@ -58,3 +70,18 @@ int main() {
     } while (choice != 777);
     return 0;
 }
+/*
+matrix - это vector<vector<int>> (вектор векторов)
+resize(n) устанавливает, что во внешнем векторе будет n элементов
+Каждый элемент внешнего вектора - это отдельный вектор (строка матрицы)
+После этой операции у нас есть n пустых строк
+matrix[0] = []  (пустой вектор)
+matrix[1] = []  (пустой вектор)  
+matrix[2] = []  (пустой вектор)
+Проходим по каждой строке (от 0 до n-1)
+matrix[i].resize(m) устанавливает, что в каждой строке будет m элементов
+Все элементы инициализируются значением по умолчанию (для int - это 0)
+matrix[0] = [0, 0, 0, ..., 0]  (m нулей)
+matrix[1] = [0, 0, 0, ..., 0]  (m нулей)
+matrix[2] = [0, 0, 0, ..., 0]  (m нулей)
+*/
